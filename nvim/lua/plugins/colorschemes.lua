857d12b566c15a1657602573956421d805dcad27
dawidgieras98 <dawid.gieras98@gmail.com>
2025-07-21 22:48:13 +0200

    some nvim tweaks

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,55 +1,13 @@
--- return {
---   "rose-pine/neovim",
---   name = "rose-pine",
---   priority = 1000,
---   config = function()
---     require("rose-pine").setup({
---       variant = "moon",
---       dark_variant = "main",
---       styles = {
---         bold = true,
---         italic = true,
---         transparency = true,
---       },
---       highlight_groups = {
---         Normal = { bg = "none" },
---         NormalNC = { bg = "none" },
---         NormalFloat = { bg = "none" },
---         FloatBorder = { bg = "none" },
---         CursorLine = { bg = "none" },
---         StatusLine = { bg = "none" },
---         SignColumn = { bg = "none" },
---         VertSplit = { bg = "none" },
---         Pmenu = { bg = "none" },
---         PmenuSel = { bg = "none" },
---         TelescopeNormal = { bg = "none" },
---         TelescopeBorder = { bg = "none" },
---       },
---     })
---
---     vim.cmd("colorscheme rose-pine")
---   end,
--- }
---
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require("catppuccin").setup({
-      transparent_background = false, -- keep this false to override selectively
-      custom_highlights = function(colors)
-        return {
-          Normal = { bg = "NONE" },
-          NormalNC = { bg = "NONE" },
-          NormalFloat = { bg = "NONE" },
-          FloatBorder = { fg = colors.lavender, bg = "NONE" },
-          Pmenu = { bg = "NONE" },
-          TelescopeNormal = { bg = "NONE" },
-          TelescopeBorder = { fg = colors.lavender, bg = "NONE" },
-        }
-      end,
+    require("github-theme").setup({
+      -- ...
     })
-    vim.cmd.colorscheme("catppuccin")
+
+    vim.cmd("colorscheme github_dark_tritanopia")
   end,
 }
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,288 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    event = "LazyFile",
+    dependencies = {
+      "mason.nvim",
+      { "mason-org/mason-lspconfig.nvim", config = function() end },
+    },
+    opts = function()
+      ---@class PluginLspOpts
+      local ret = {
+        diagnostics = {
+          underline = true,
+          update_in_insert = false,
+          virtual_text = {
+            spacing = 4,
+            source = "if_many",
+            prefix = "●",
+          },
+          severity_sort = true,
+          signs = {
+            text = {
+              [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+              [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+              [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+              [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+            },
+          },
+        },
+        inlay_hints = {
+          enabled = true,
+          exclude = { "vue" },
+        },
+        codelens = {
+          enabled = false,
+        },
+        capabilities = {
+          workspace = {
+            fileOperations = {
+              didRename = true,
+              willRename = true,
+            },
+          },
+        },
+        format = {
+          formatting_options = nil,
+          timeout_ms = nil,
+        },
+        ---@type lspconfig.options
+        servers = {
+          lua_ls = {
+            settings = {
+              Lua = {
+                workspace = {
+                  checkThirdParty = false,
+                },
+                codeLens = {
+                  enable = true,
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
+                doc = {
+                  privateName = { "^_" },
+                },
+                hint = {
+                  enable = true,
+                  setType = false,
+                  paramType = true,
+                  paramName = "Disable",
+                  semicolon = "Disable",
+                  arrayIndex = "Disable",
+                },
+              },
+            },
+          },
+          volar = {
+            init_options = {
+              vue = {
+                hybridMode = false,
+              },
+            },
+            settings = {
+              typescript = {
+                inlayHints = {
+                  enumMemberValues = { enabled = true },
+                  functionLikeReturnTypes = { enabled = true },
+                  propertyDeclarationTypes = { enabled = true },
+                  parameterTypes = {
+                    enabled = true,
+                    suppressWhenArgumentMatchesName = true,
+                  },
+                  variableTypes = { enabled = true },
+                },
+              },
+            },
+          },
+          gopls = {
+            settings = {
+              gopls = {
+                analyses = {
+                  unusedparams = true,
+                },
+                staticcheck = true,
+                usePlaceholders = true,
+                completeUnimported = true,
+                gofumpt = true,
+              },
+            },
+          },
+        },
+        ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+        setup = {
+          tsserver = function(_, opts)
+            local mason_registry = require("mason-registry")
+            local vue_ls_path = mason_registry.get_package("vue-language-server"):get_install_path()
+              .. "/node_modules/@vue/language-server"
+
+            opts.init_options = {
+              plugins = {
+                {
+                  name = "@vue/typescript-plugin",
+                  location = vue_ls_path,
+                  languages = { "vue" },
+                },
+              },
+            }
+
+            opts.filetypes = {
+              "typescript",
+              "javascript",
+              "javascriptreact",
+              "typescriptreact",
+            }
+
+            -- ⚠️ Do NOT return anything here!
+          end,
+          volar = function(_, _)
+            require("lazyvim.util").lsp.on_attach(function(client, _)
+              if client.name == "volar" then
+                client.server_capabilities.documentFormattingProvider = false
+              end
+            end)
+          end,
+        },
+      }
+      return ret
+    end,
+    config = function(_, opts)
+      LazyVim.format.register(LazyVim.lsp.formatter())
+
+      LazyVim.lsp.on_attach(function(client, buffer)
+        require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
+      end)
+
+      LazyVim.lsp.setup()
+      LazyVim.lsp.on_dynamic_capability(require("lazyvim.plugins.lsp.keymaps").on_attach)
+
+      if vim.fn.has("nvim-0.10.0") == 0 then
+        if type(opts.diagnostics.signs) ~= "boolean" then
+          for severity, icon in pairs(opts.diagnostics.signs.text) do
+            local name = vim.diagnostic.severity[severity]:lower():gsub("^%l", string.upper)
+            name = "DiagnosticSign" .. name
+            vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+          end
+        end
+      end
+
+      if vim.fn.has("nvim-0.10") == 1 then
+        if opts.inlay_hints.enabled then
+          LazyVim.lsp.on_supports_method("textDocument/inlayHint", function(client, buffer)
+            if
+              vim.api.nvim_buf_is_valid(buffer)
+              and vim.bo[buffer].buftype == ""
+              and not vim.tbl_contains(opts.inlay_hints.exclude, vim.bo[buffer].filetype)
+            then
+              vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
+            end
+          end)
+        end
+
+        if opts.codelens.enabled and vim.lsp.codelens then
+          LazyVim.lsp.on_supports_method("textDocument/codeLens", function(_, buffer)
+            vim.lsp.codelens.refresh()
+            vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+              buffer = buffer,
+              callback = vim.lsp.codelens.refresh,
+            })
+          end)
+        end
+      end
+
+      if
+        type(opts.diagnostics.virtual_text) == "table"
+        and opts.diagnostics.virtual_text.prefix == "icons"
+      then
+        opts.diagnostics.virtual_text.prefix = vim.fn.has("nvim-0.10.0") == 0 and "●"
+          or function(diagnostic)
+            local icons = LazyVim.config.icons.diagnostics
+            for d, icon in pairs(icons) do
+              if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+                return icon
+              end
+            end
+          end
+      end
+
+      vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+
+      local servers = opts.servers
+      local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      local has_blink, blink = pcall(require, "blink.cmp")
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        has_cmp and cmp_nvim_lsp.default_capabilities() or {},
+        has_blink and blink.get_lsp_capabilities() or {},
+        opts.capabilities or {}
+      )
+
+      local function setup(server)
+        local server_opts = vim.tbl_deep_extend("force", {
+          capabilities = vim.deepcopy(capabilities),
+        }, servers[server] or {})
+
+        if server_opts.enabled == false then
+          return
+        end
+
+        if opts.setup[server] then
+          if opts.setup[server](server, server_opts) then
+            return
+          end
+        elseif opts.setup["*"] then
+          if opts.setup["*"](server, server_opts) then
+            return
+          end
+        end
+
+        require("lspconfig")[server].setup(server_opts)
+      end
+
+      local have_mason, mlsp = pcall(require, "mason-lspconfig")
+      local all_mslp_servers = {}
+      if have_mason then
+        all_mslp_servers =
+          vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
+      end
+
+      local ensure_installed = {} ---@type string[]
+      for server, server_opts in pairs(servers) do
+        if server_opts then
+          server_opts = server_opts == true and {} or server_opts
+          if server_opts.enabled ~= false then
+            if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
+              setup(server)
+            else
+              ensure_installed[#ensure_installed + 1] = server
+            end
+          end
+        end
+      end
+
+      if have_mason then
+        mlsp.setup({
+          ensure_installed = vim.tbl_deep_extend(
+            "force",
+            ensure_installed,
+            LazyVim.opts("mason-lspconfig.nvim").ensure_installed or {}
+          ),
+          handlers = { setup },
+        })
+      end
+
+      if LazyVim.lsp.is_enabled("denols") and LazyVim.lsp.is_enabled("vtsls") then
+        local is_deno = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
+        LazyVim.lsp.disable("vtsls", is_deno)
+        LazyVim.lsp.disable("denols", function(root_dir, config)
+          if not is_deno(root_dir) then
+            config.settings.deno.enable = false
+          end
+          return false
+        end)
+      end
+    end,
+  },
+}
