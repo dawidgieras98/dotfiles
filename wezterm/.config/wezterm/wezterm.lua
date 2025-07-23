34a2d5db40fc4ff49588cdc88caa084bb8bd80dc
dgieras <dgieras@bettercollective.com>
2025-07-23 11:18:06 +0200

    change cursor type

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -20,7 +20,7 @@ return {
 	enable_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = true,
 	use_fancy_tab_bar = true,
-
+	default_cursor_style = "BlinkingBar",
 	-- Window
 	window_padding = {
 		left = 6,
