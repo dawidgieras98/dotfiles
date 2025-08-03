a50ccbd6e2a78afb64046ade87134591851afc43
dawidgieras98 <dawid.gieras98@gmail.com>
2025-08-03 20:13:49 +0200

    add keybindings to be able to move inside insert mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -11,3 +11,7 @@ vim.keymap.set("n", "ww", ":w<CR>", { noremap = true, silent = true })
 -- KEYMAPS WITH MOVE LINE UP AND DOWN ARE MOVED TO LSP FILE WHERE LSP ON ATTACH HOOK IS MADE, ALSO SHIFT D WORKS AS HOVER IN LSP INSTEAD OF SHIFT K
 -- vim.keymap.set("n", "K", ":m .-2<CR>==", { noremap = true, silent = true })
 -- vim.keymap.set("n", "J", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-Left>", "<C-o>h", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-Right>", "<C-o>l", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-Up>", "<C-o>k", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-Down>", "<C-o>j", { noremap = true, silent = true })
--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -11,6 +11,26 @@ local keys = {
 		mods = "ALT",
 		action = wezterm.action.ActivateTabRelative(1),
 	},
+	{
+		key = "H",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.SendKey({ key = "LeftArrow", mods = "CTRL" }),
+	},
+	{
+		key = "L",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.SendKey({ key = "RightArrow", mods = "CTRL" }),
+	},
+	{
+		key = "K",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.SendKey({ key = "UpArrow", mods = "CTRL" }),
+	},
+	{
+		key = "J",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.SendKey({ key = "DownArrow", mods = "CTRL" }),
+	},
 }
 
 return keys
